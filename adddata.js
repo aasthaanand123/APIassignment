165e5446aacd2b1a21b302c13d4d1b16e9b03001
aastha anand <aasthaanand193@hotmail.com>
2023-09-10 08:52:52 -0700

    functionality added

--- adddata.js
+++ adddata.js
@@ -1,30 +1,51 @@
-const countries=document.querySelector(".countrieslist");
-const states=document.querySelector(".stateslist");
-const cities=document.querySelector(".citieslist");
+function statesofcountry(el){
+    el.forEach(element=>{
+        element.addEventListener("click",(e)=>{
+          let countryid=element.id;
+          let filteredstates=statearray.filter((state)=>state.country_id==countryid);
+          states.innerHTML=``;
+          addToStates(filteredstates)
+          addToCities(cityarray);
+        })
+    })
+}
+function citiesofstates(el){
+    el.forEach(element=>{
+        element.addEventListener("click",(e)=>{
+          let stateid=element.id;
+          let filteredcities=cityarray.filter((city)=>city.state_id==stateid);
+          cities.innerHTML=``;
+          addToCities(filteredcities)
+        })
+    })
+}
+removehide(dropdown_button);
 function addToCountries(data){
     data.forEach(element=>{
         let newel=document.createElement("li");
-        newel.innerHTML=`<li><a href="" class="clickcountry">${element.name}</a></li>`;
-        newel.classList.add("country",`${element.id}`)
+        newel.innerHTML=`<li><a href="" class="clickcountry country" id="${element.id}">${element.name}</a></li>`;
+        
         countries.appendChild(newel);
     })
+    statesofcountry(document.querySelectorAll(".country"))
     removedefault(document.querySelectorAll(".clickcountry"));
 }
 function addToStates(data){
     data.forEach(element=>{
         let newel=document.createElement("li");
-        newel.innerHTML=`<li><a href="" class="clickstate">${element.name}</a></li>`;
-        newel.classList.add("state",`${element.id}`)
+        newel.innerHTML=`<li><a href="" class="clickstate state" id="${element.id}">${element.name}</a></li>`;
+        
         states.appendChild(newel);
     })
+    citiesofstates(document.querySelectorAll(".state"));
     removedefault(document.querySelectorAll(".clickstate"));
 }
 function addToCities(data){
     data.forEach(element=>{
         let newel=document.createElement("li");
-        newel.innerHTML=`<li><a href="" class="clickcity">${element.name}</a></li>`;
-        newel.classList.add("city",`${element.id}`)
+        newel.innerHTML=`<li><a href="" class="clickcity city el" id="${element.id}">${element.name}</a></li>`;
         cities.appendChild(newel);
     })
     removedefault(document.querySelectorAll(".clickcity"));
+
 }
---
+++ elementselector.js
@@ -0,0 +1,7 @@
+const countries=document.querySelector(".countrieslist");
+const states=document.querySelector(".stateslist");
+const cities=document.querySelector(".citieslist");
+const dropdown_button=document.querySelectorAll(".drop-btn");
+let countryarray=[];
+let statearray=[];
+let cityarray=[];
